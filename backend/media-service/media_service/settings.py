909be8a3ab9b681a5bb6d47f81662822b1e785ee
kirigaya122 <swilimbaowen@gmail.com>
2026-05-09 11:16:50 +0200

    Added postgresql configurations and setup db connection and created the media model

--- backend/media-service/media_service/settings.py
+++ backend/media-service/media_service/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'media_service.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mediadb',
+        'USER': 'media',
+        'PASSWORD': 'mediapass',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 

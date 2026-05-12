8c0aa9d9957cfbdaa22717ed95704d6743990ce2
kirigaya122 <swilimbaowen@gmail.com>
2026-05-12 13:12:36 +0200

    Added map page and fixed location getting on user report incident page

--- frontend /src/pages/user/settings/sub_setting_pages/my_reports/MyReport.jsx
+++ frontend /src/pages/user/settings/sub_setting_pages/my_reports/MyReport.jsx
@@ -43,6 +43,7 @@ const MyReports = () => {
         return filter.sortTime === "latest" ? dateB - dateA : dateA - dateB;
       });
   }, [filter, reports]);
+  console.log(reports);
 
   if (!reports && !allDeparts) return <div>.... isloading</div>;
 

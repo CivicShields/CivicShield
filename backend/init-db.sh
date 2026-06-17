05a20a58d968d0e855e61c4297bdd25af6500322
kirigaya122 <swilimbaowen@gmail.com>
2026-06-17 15:27:18 +0200

    Fixed docker issue

--- backend/init-db.sh
+++ backend/init-db.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
 
-# Create databases if they don't exist
+# Create databases if they don't exist (connect to the 'postgres' database)
 for db in auth_db incident_db dept_db notification_db media_db; do
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d postgres <<-EOSQL
         SELECT 'CREATE DATABASE $db'
         WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$db')\gexec
 EOSQL
 done
 
 # Enable PostGIS extension on incident_db
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname=incident_db <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -d incident_db <<-EOSQL
     CREATE EXTENSION IF NOT EXISTS postgis;
     CREATE EXTENSION IF NOT EXISTS postgis_topology;
 EOSQL
